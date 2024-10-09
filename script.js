df1951f595d19bdd2c39ff2cb92c57693bbc36f2
tdatnef <phamdatqn2006@gmail.com>
2024-10-09 18:20:29 +0700

    html

---
+++ script.js
@@ -0,0 +1,6 @@
+const callBtn = document.getElementById("callBtn");
+const hangupBtn = document.getElementById("hangupBtn");
+const loginBtn = document.getElementById("loginBtn");
+const userId = document.getElementById("userId");
+const accessToken = document.getElementById("accessToken");
+const callStatus = document.getElementById("callStatus");
