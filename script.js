09b6af59d1d232af0573c71a0c531cc60b58c1ba
HoangPhi2004 <22H1120047@ut.edu.vn>
2024-10-12 21:38:46 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const callBtn = document.getElementById("callBtn");
-const hangupBtn = document.getElementById("hangupBtn");
-const loginBtn = document.getElementById("loginBtn");
-const userId = document.getElementById("userId");
-const accessToken = document.getElementById("accessToken");
-const callStatus = document.getElementById("callStatus");
